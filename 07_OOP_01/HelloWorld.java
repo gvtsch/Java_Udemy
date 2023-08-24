a4e44183f03998c175d0c3f1fb67f7a11863ab3c
gvtsch <kempkes.christoph@gmail.com>
2023-08-24 17:47:59 +0200

    Konstruktor

--- 07_OOP_01/HelloWorld.java
+++ 07_OOP_01/HelloWorld.java
@@ -4,16 +4,11 @@ public class HelloWorld {
     
     public static void main(String[] args) {
         
-        Player p1 = new Player();
-        p1.name = "Erik";
-        p1.numberOfCards = 12;
-
-        Player p2 = new Player();
-        p2.name = "Monika";
-        p2.numberOfCards = 13;
+        Player p1 = new Player("Erik", 11);
+        Player p2 = new Player("Monika", 13);        
 
         p1.printPlayerInfo();
         p2.printPlayerInfo();    
-        
+
     }
 }
--- 07_OOP_01/Player.java
+++ 07_OOP_01/Player.java
@@ -3,6 +3,11 @@ public class Player {
     String name;
     int numberOfCards;
 
+    Player(String n, int c) {
+        name = n;
+        numberOfCards = c;
+    }
+
     public void printPlayerInfo() {
         System.out.println(name + "(" + numberOfCards + ")");
     }
