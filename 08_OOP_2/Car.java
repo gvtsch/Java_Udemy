036ac53f611c96a750464675e7e5d93bc0327736
gvtsch <kempkes.christoph@gmail.com>
2023-08-24 19:08:57 +0200

    Main

--- 08_OOP_2/Car.java
+++ 08_OOP_2/Car.java
@@ -1,4 +1,13 @@
 public class Car {
     private String brand;
     private int hp;
+
+    Car(String brand, int hp){
+        this.brand = brand;
+        this.hp = hp;
+    }
+
+    public void drive(){
+        System.out.println("Ein " + this.brand + " fährt mit " + this.hp + " PS.");
+    }
 }
--- 08_OOP_2/HelloWorld.java
+++ 08_OOP_2/HelloWorld.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        
+        Car c = new Car("BMW", 250);
+        c.drive();
     }
 }
